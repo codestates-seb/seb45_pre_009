26afd0d7862c704369975e3a5e99f4688d62dff6
jungseo <jungwook22@gmail.com>
2023-08-09 22:11:04 +0900

    :art: responseDto 적용 및 모든 질문 조회 시 내용을 제외한 항목들 페이지네이션으로 응답 구현

--- server/src/main/java/com/gujo/stackoverflow/question/controller/QuestionController.java
+++ server/src/main/java/com/gujo/stackoverflow/question/controller/QuestionController.java
@@ -9,17 +9,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
 @RequestMapping("/questions")
 public class QuestionController {
 
-    private QuestionService questionService;
+    private final QuestionService questionService;
 
-    private QuestionMapper mapper;
+    private final QuestionMapper mapper;
 
-    public QuestionController(QuestionMapper mapper) {
+    public QuestionController(QuestionService questionService, QuestionMapper mapper) {
+        this.questionService = questionService;
         this.mapper = mapper;
     }
 
@@ -27,18 +30,28 @@ public class QuestionController {
     public ResponseEntity postQuestion(@RequestBody QuestionDto.PostDto postDto) {
         Question question = mapper.questionPostDtoToQuestion(postDto);
         Question createdQuestion = questionService.createQuestion(question);
-        return new ResponseEntity(createdQuestion, HttpStatus.CREATED);
+        QuestionDto.ResponseDto responseQuestion = mapper.questionToQuestionResponseDto(createdQuestion);
+        return new ResponseEntity(responseQuestion, HttpStatus.CREATED);
     }
 
-    @GetMapping()
+    @GetMapping
     public ResponseEntity getQuestions(Pageable pageable) {
-        List<Question> questions = questionService.getAllQuestions(pageable);
-        return new ResponseEntity(questions, HttpStatus.OK);
+        List<Question> questions = questionService.getQuestions(pageable);
+
+//        전체 질문 조회 시 게시물의 제목 이름 등 표시 -> 내용 미포함
+//        List<Question>으로 질문들을 받아오고 반복자를 활용해 전체질문 조회 요청용 응답 DTO로 변환
+        List<QuestionDto.getQuestionsResponseDto> result = new ArrayList<>();
+        for (Question question : questions) {
+            result.add(mapper.questionToGetQuestionsResponseDto(question));
+        }
+
+        return new ResponseEntity(result, HttpStatus.OK);
     }
 
     @GetMapping("/{question-id}")
     public ResponseEntity getQuestion(@PathVariable("question-id") Long questionId) {
-        Question findQuestion = questionService.getQuestion(questionId);
-        return new ResponseEntity(findQuestion, HttpStatus.OK);
+        Question question = questionService.getQuestion(questionId);
+        QuestionDto.ResponseDto responseQuestion = mapper.questionToQuestionResponseDto(question);
+        return new ResponseEntity(responseQuestion, HttpStatus.OK);
     }
 }
