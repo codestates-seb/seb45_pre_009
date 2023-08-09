41b845c8736fc32fcb474a85fa32ed9ba4b44ce9
jungseo <jungwook22@gmail.com>
2023-08-09 22:16:02 +0900

    :sparkles: responseDto, 모든 질문 조회 응답용 DTO 생성 및 getter setter 추가..

--- server/src/main/java/com/gujo/stackoverflow/question/dto/QuestionDto.java
+++ server/src/main/java/com/gujo/stackoverflow/question/dto/QuestionDto.java
@@ -1,14 +1,49 @@
 package com.gujo.stackoverflow.question.dto;
 
+import com.gujo.stackoverflow.member.entity.Member;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
 public class QuestionDto {
 
-    public class PostDto {
+    @Getter
+    public static class PostDto {
+        private String title;
+
+        private String content;
+    }
+
+    @Getter
+    @Setter
+    public static class ResponseDto {
+        private Long questionId;
+
+        private Member member;
+
         private String title;
 
         private String content;
 
-        private LocalDateTime createdAt = LocalDateTime.now();
+        private Long point;
+
+        private LocalDateTime createdAt;
+
+        private LocalDateTime modifiedAt;
+    }
+
+    @Getter
+    @Setter
+    public static class getQuestionsResponseDto {
+        private Long questionId;
+
+        private Member member;
+
+        private String title;
+
+        private LocalDateTime createdAt;
+
+        private LocalDateTime modifiedAt;
     }
 }
