caf5c7a71570ae91b60df4a6737a2e6eb9e2e9e0
jungseo <jungwook22@gmail.com>
2023-08-09 22:48:36 +0900

    :recycle: 변수명 리팩토링 및 update delete 기능 구현

--- server/src/main/java/com/gujo/stackoverflow/question/controller/QuestionController.java
+++ server/src/main/java/com/gujo/stackoverflow/question/controller/QuestionController.java
@@ -10,33 +10,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 @RestController
 @RequestMapping("/questions")
 public class QuestionController {
 
-    private final QuestionService questionService;
+    private final QuestionService service;
 
     private final QuestionMapper mapper;
 
     public QuestionController(QuestionService questionService, QuestionMapper mapper) {
-        this.questionService = questionService;
+        this.service = questionService;
         this.mapper = mapper;
     }
 
     @PostMapping
     public ResponseEntity postQuestion(@RequestBody QuestionDto.PostDto postDto) {
         Question question = mapper.questionPostDtoToQuestion(postDto);
-        Question createdQuestion = questionService.createQuestion(question);
-        QuestionDto.ResponseDto responseQuestion = mapper.questionToQuestionResponseDto(createdQuestion);
-        return new ResponseEntity(responseQuestion, HttpStatus.CREATED);
+        Question created = service.createQuestion(question);
+
+        QuestionDto.ResponseDto responseDto = mapper.questionToQuestionResponseDto(created);
+        return new ResponseEntity(responseDto, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity getQuestions(Pageable pageable) {
-        List<Question> questions = questionService.getQuestions(pageable);
+        List<Question> questions = service.getQuestions(pageable);
 
 //        전체 질문 조회 시 게시물의 제목 이름 등 표시 -> 내용 미포함
 //        List<Question>으로 질문들을 받아오고 반복자를 활용해 전체질문 조회 요청용 응답 DTO로 변환
@@ -50,8 +50,26 @@ public class QuestionController {
 
     @GetMapping("/{question-id}")
     public ResponseEntity getQuestion(@PathVariable("question-id") Long questionId) {
-        Question question = questionService.getQuestion(questionId);
-        QuestionDto.ResponseDto responseQuestion = mapper.questionToQuestionResponseDto(question);
-        return new ResponseEntity(responseQuestion, HttpStatus.OK);
+        Question question = service.getQuestion(questionId);
+
+        QuestionDto.ResponseDto responseDto = mapper.questionToQuestionResponseDto(question);
+        return new ResponseEntity(responseDto, HttpStatus.OK);
+    }
+
+    @PatchMapping("/{question-id}")
+    public ResponseEntity patchQuestion(@PathVariable("question-id") Long questionId,
+                                        @RequestBody QuestionDto.PatchDto patchDto) {
+        Question question = mapper.questionPatchDtoToQuestion(patchDto);
+        Question updated = service.updateQuestion(questionId, question);
+
+        QuestionDto.ResponseDto responseDto = mapper.questionToQuestionResponseDto(updated);
+        return new ResponseEntity(responseDto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{question-id}")
+    public ResponseEntity deleteQuestion(@PathVariable("question-id") Long questionId) {
+        service.deleteQuestion(questionId);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
