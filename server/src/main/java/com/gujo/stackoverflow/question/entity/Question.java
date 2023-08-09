5cae4cd28752d681017753fa69fd896f72ef166b
jungseo <jungwook22@gmail.com>
2023-08-09 16:10:16 +0900

    :recycle: question 리팩토링

--- server/src/main/java/com/gujo/stackoverflow/question/entity/Question.java
+++ server/src/main/java/com/gujo/stackoverflow/question/entity/Question.java
@@ -1,19 +1,23 @@
 package com.gujo.stackoverflow.question.entity;
 
-import com.gujo.stackoverflow.user.entity.User;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class Question {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long questionId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionId;
 
     @ManyToOne // 유저 한명이 여러 질문 작성 가능 -> N:1
     @JoinColumn(name = "USER_ID") // 현재 테이블에서 외래키에 해당하는 컬럼의 이름
-    private User user;
+    private Member member;
 
     @Column(nullable = false, length = 50)
     private String title;
@@ -22,7 +26,7 @@ public class Question {
     private String content;
 
     @Column(nullable = false)
-    private long point;
+    private Long point;
 
     @Column(nullable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
