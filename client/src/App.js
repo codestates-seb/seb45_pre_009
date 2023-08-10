082c71da5c3648cc602586b5cc62a092df4ea323
RunnerJinHyeon <jinee0903@gmail.com>
2023-08-10 11:17:05 +0900

    ✨ 카카오로그인 컴포넌트

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-
 import './App.css';
+import KakaoLogin from "./components/Login/KakaoLogin"
 
 function App() {
   return (
-    <></>
+    <>
+    <KakaoLogin/>
+    </>
   );
 }
 
 export default App;
+
