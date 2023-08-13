e84d6c512d9093c1ad60048fec4298937e706fd4
seoljuyeon <sxxljxxxxn@gmail.com>
2023-08-14 03:13:34 +0900

    :sparkles: SecurityConfiguration 추가

---
+++ server/src/main/java/com/gujo/stackoverflow/config/SecurityConfiguration.java
@@ -0,0 +1,51 @@
+package com.gujo.stackoverflow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .headers().frameOptions().sameOrigin()
+                .and()
+                .csrf().disable()   // 안 하면 403 에러 뜸 -> 접속 불가능
+                .cors(withDefaults())
+                .formLogin().disable()  // 우린 CSR 방식이라 비활성화
+                .httpBasic().disable()
+                .authorizeHttpRequests(authorize -> authorize   // 이건 JWT 적용 전이라... 이따가 지우기~
+                        .anyRequest().permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {     // 구체적인 CORS 정책 설정~
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
