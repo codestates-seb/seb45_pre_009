e777252665aaf126e5d415d05bb9d5407fc38915
seoljuyeon <sxxljxxxxn@gmail.com>
2023-08-14 23:25:03 +0900

    :sparkles: ( MemberRepository) 데이터 조회 메서드 추가

--- server/src/main/java/com/gujo/stackoverflow/member/repository/MemberRepository.java
+++ server/src/main/java/com/gujo/stackoverflow/member/repository/MemberRepository.java
@@ -7,5 +7,8 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByDisplayName(String displayName);
+    // 로그인 디티오에 유저네임을 이메일로 로그인 할 테니 디스플레이네임은..
+
+    Optional<Member> findByEmail(String email);
 
 }
