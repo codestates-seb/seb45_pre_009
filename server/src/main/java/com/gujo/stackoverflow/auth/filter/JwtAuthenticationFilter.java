4a53696588ca40e440a7b64ba43c560955eb3b41
seoljuyeon <sxxljxxxxn@gmail.com>
2023-08-15 03:29:09 +0900

    :sparkles: JwtAuthenticationFilter 구현

---
+++ server/src/main/java/com/gujo/stackoverflow/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,86 @@
+package com.gujo.stackoverflow.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gujo.stackoverflow.auth.dto.LoginDto;
+import com.gujo.stackoverflow.auth.jwt.JwtTokenizer;
+import com.gujo.stackoverflow.member.entity.Member;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+// 로그인 인증 요청 처리하는 엔트리 포인트
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    // ^ 얘 DI 해서 Manager ( -> provider) -> MemberDetailsService
+    private final JwtTokenizer jwtTokenizer;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtTokenizer jwtTokenizer) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenizer = jwtTokenizer;
+    }
+
+    @SneakyThrows // 명시적 예외 처리 생략 가능, 안 쓰면 귀찮아짐~
+    @Override
+    // 내부에서 인증 시도하는 메서드
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        ObjectMapper objectMapper = new ObjectMapper();     // ObjectMapper : Dto 클래스로 역직렬화 ( json -> dto)
+        LoginDto loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class);
+
+        // username, pw 정보 포함한 토큰 생성 ( 인증 전임)
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+
+        // 위 토큰을 Manager 한테 전달하면서 인증 처리 위임
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    // 인증 성공시 호출
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) {    // < 얜 인증 됐음
+        Member member = (Member) authResult.getPrincipal();     // 인증된 정보를 통해서 객체를 얻을 수 있음
+
+        String accessToken = delegateAccessToken(member);
+        String refreshToken = delegateRefreshToken(member);
+
+        response.setHeader("Authorization", "Bearer " + accessToken);
+        response.setHeader("Refresh", refreshToken);
+    }
+
+    private String delegateAccessToken(Member member) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", member.getEmail());
+        claims.put("roles", member.getRoles());
+
+        String subject = member.getEmail();
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String accessToken = jwtTokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+
+        return accessToken;
+    }
+
+    private String delegateRefreshToken(Member member) {
+        String subject = member.getEmail();
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getRefreshTokenExpirationMinutes());
+
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String refreshToken = jwtTokenizer.generateRefreshToken(subject, expiration, base64EncodedSecretKey);
+
+        return refreshToken;
+    }
+}
