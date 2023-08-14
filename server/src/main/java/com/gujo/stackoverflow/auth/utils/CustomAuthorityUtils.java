bdce7dd229353cc2a48b38e8b36cea1838cffb02
seoljuyeon <sxxljxxxxn@gmail.com>
2023-08-14 23:21:37 +0900

    :sparkles: 권한 부여 유틸 클래스 코드 수정

--- server/src/main/java/com/gujo/stackoverflow/auth/utils/CustomAuthorityUtils.java
+++ server/src/main/java/com/gujo/stackoverflow/auth/utils/CustomAuthorityUtils.java
@@ -3,8 +3,10 @@ package com.gujo.stackoverflow.auth.utils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CustomAuthorityUtils {
     @Value("${mail.address.admin}")     // 환경변수 설정 해뒀어요
@@ -14,17 +16,18 @@ public class CustomAuthorityUtils {
     private final List<GrantedAuthority> ADMIN_ROLES = AuthorityUtils.createAuthorityList("ROLE_ADMIN", "ROLE_USER");
     private final List<GrantedAuthority> USER_ROLES = AuthorityUtils.createAuthorityList("ROLE_USER");
 
-    // 관리자 이메일 일치 여부 판단
-    public List<GrantedAuthority> createAuthorities(String email) {
-        if( email.equals(adminMailAddress)) {
-            return ADMIN_ROLES;
-        }
-        return USER_ROLES;
-    }
-
     // DB 저장용
     private final List<String> ADMIN_ROLES_STRING = List.of("ADMIN", "USER");
     private final List<String> USER_ROLES_STRING = List.of("USER");
+
+    // DB 정보 통해 권한 정보 생성
+    public List<GrantedAuthority> createAuthorities(List<String> roles) {
+        List<GrantedAuthority> authorities = roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
+
+        return authorities;
+    }
 
     public List<String> createRoles(String email) {
         if( email. equals(adminMailAddress)) {
