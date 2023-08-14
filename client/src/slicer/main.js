a2996f2fa50645d8d36ef5128cc2887ce691d39d
ColmiismaL <colruck32@gmail.com>
2023-08-14 13:48:46 +0900

    일단 중간 올리기

--- client/src/slicer/main.js
+++ client/src/slicer/main.js
@@ -7,15 +7,42 @@ import { getApi } from '../api/api.js';
 export const fetchData = createAsyncThunk('data/fetchData', async (path) => {
     return await getApi(path);
     });
+
+export const fetchUserById = createAsyncThunk('data/fetchUserById', async (userId) => {
+    const response = await getApi(`user/${userId}`); 
+    console.log('Response:', response);
+    return { userId, user: response.data };
+    });
+
+    // export const fetchUserById = createAsyncThunk('data/fetchUserById', async (userId) => {
+    //     // 로컬 스토리지에서 사용자 정보를 먼저 확인
+    //     const cachedUser = localStorage.getItem(`user_${userId}`);
+    //     if (cachedUser) {
+    //         return { userId, user: JSON.parse(cachedUser) };
+    //     }
+
+    //     const response = await getApi(`user/${userId}`);
+    //     console.log('Response:', response);
+    //     const user = response.data;
+    
+    //     // 로컬 스토리지에 사용자 정보 저장
+    //     localStorage.setItem(`user_${userId}`, JSON.stringify(user));
+        
+    //     return { userId, user };
+    // });
     
 export const dataSlice = createSlice({
         name: 'data',
         initialState: {
         items: [],
+        users: [],
         status: 'idle',
         },
         extraReducers: (builder) => {
         builder
+            .addCase(fetchUserById.fulfilled, (state, action) => {
+            state.users[action.payload.userId] = action.payload.user; // 사용자 ID를 키로 사용하여 사용자 정보 저장
+            })
             .addCase(fetchData.pending, (state) => {
             state.status = 'loading';
             })
