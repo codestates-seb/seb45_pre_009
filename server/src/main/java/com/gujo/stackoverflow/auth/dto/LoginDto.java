f04d7f040d617ebbae5f4e846c391e0aaed04242
seoljuyeon <sxxljxxxxn@gmail.com>
2023-08-15 02:48:24 +0900

    :sparkles: ( LoginDto) 애너테이션 추가( Getter, Setter)

--- server/src/main/java/com/gujo/stackoverflow/auth/dto/LoginDto.java
+++ server/src/main/java/com/gujo/stackoverflow/auth/dto/LoginDto.java
@@ -1,5 +1,9 @@
 package com.gujo.stackoverflow.auth.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class LoginDto {
     private String username;
     private String password;
