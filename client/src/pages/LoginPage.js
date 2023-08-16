a6fc90ea7d72cd84ee9f726244bc624c7bfbcd8e
RunnerJinHyeon <jinee0903@gmail.com>
2023-08-16 11:40:51 +0900

    🎨 로그인 폼 디자인

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -5,6 +5,7 @@ import KakaoLogin from "../components/Login/KakaoLogin";
 
 import kakaoIcon from "../images/Kakao.png";
 import googleIcon from "../images/google.png";
+import stackIcon from "../images/stackicon.png"
 
 
 import {useState} from "react";
@@ -21,6 +22,7 @@ const LoginPage = () => {
 
 
         <div className="flex flex-col justify-center items-center min-h-screen">
+        <div className="flex justify-center items-center"><img className="w-[30px] h-[30px]" src={stackIcon} alt="스택오버플로우 아이콘"/></div>
         <button className="w-[300px] h-15 bg-[#FFFFFF] rounded-xl  left-2.5 border-2 border-[#E6E6E6] flex items-center mt-4 mb-2">
         <img className="w-10 h-10 mx-5" src={googleIcon} alt=" 구글 로그인 " />
         <div className="flex flex-col items-start">
@@ -36,6 +38,8 @@ const LoginPage = () => {
         <div className="w-[300px] h-[350px] rounded-xl  left-2.5  border-2 border-[#E6E6E6] flex items-center justify-center mt-4 md-4 bg-[white]">
         <NormalLogin/>
         </div>
+        <span className="text-xs mt-10">Don't have an account? Sign up</span>
+        <span className="text-xs">Are you an employer? Sign up on Talent</span>
         </div>
         </div>
         </>
