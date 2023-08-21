52f906489b76d9682078ea9902c4e6097a16a7b5
seoljuyeon <sxxljxxxxn@gmail.com>
2023-08-21 09:19:16 +0900

    :sparkles: Jwt 예외처리

---
+++ server/src/main/java/com/gujo/stackoverflow/auth/handler/MemberAccessDeniedHandler.java
@@ -0,0 +1,22 @@
+package com.gujo.stackoverflow.auth.handler;
+
+import com.gujo.stackoverflow.auth.utils.ErrorResponder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class MemberAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException, IOException {
+        ErrorResponder.sendErrorResponse(response, HttpStatus.FORBIDDEN);
+        log.warn("Forbidden error happened: {}", accessDeniedException.getMessage());
+    }
+}
