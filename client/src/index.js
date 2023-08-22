2d4f2369992072ffc8a61171190df6ce2c65df3e
RunnerJinHyeon <jinee0903@gmail.com>
2023-08-22 14:52:11 +0900

    배포준비2

--- client/src/index.js
+++ client/src/index.js
@@ -5,19 +5,19 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { BrowserRouter } from "react-router-dom";
-import persistStore from "redux-persist/es/persistStore"
-import { PersisGate } from "redux-persist/integration/react"
 
-export let persistor = persistStore(store);
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     
     <Provider store={store}>
-      <PersisGate loading={null} persistor={persistor}>
+      
         <BrowserRouter basename={process.env.PUBLIC_URL}>
           <App />
         </BrowserRouter>
-      </PersisGate>
+      
     </Provider>
     
 );
